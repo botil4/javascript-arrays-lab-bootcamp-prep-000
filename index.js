39626805d160e4f527443b9075e1e8020b6b5d7c
Travis Clarkson <tk1667@gmail.com>
2018-02-04 21:32:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const app = "I don't do much."
-function Arrays() {
-  window.kittens = ["Milo", "Otis", "Garfield"]
-}
+
+window.kittens = ["Milo", "Otis", "Garfield"]
 
 function kittens() {
   var kittens = ["Milo", "Otis", "Garfield"]
