56fcbf5ffcf21f154d80d33d7b33d048d4440626
Travis Clarkson <tk1667@gmail.com>
2018-02-04 21:40:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+
+
