5381ff38ce1e34c1f3bda3f537a5c6b92aed2e55
Travis Clarkson <tk1667@gmail.com>
2018-02-04 21:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,35 +5,35 @@ kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
-
+kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
-
+kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyRemoveLastKitten() {
   kittens.pop()
 }
-
+kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
-
+kittens = ["Milo", "Otis", "Garfield"]
 function appendKitten(name) {
   var kittensNew = [...kittens, name]
   return kittensNew
 }
-
+kittens = ["Milo", "Otis", "Garfield"]
 function prependKitten(name) {
   var kittensNew = [name, ...kittens]
   return kittensNew
 }
-
+kittens = ["Milo", "Otis", "Garfield"]
 function removeLastKitten() {
   var kittensNew = kittens.slice(-1)
   return kittensNew
 }
-
+kittens = ["Milo", "Otis", "Garfield"]
 function removeFirstKitten() {
-  var kittensNew = kittens.slice
+  var kittensNew = kittens.slice()
   return kittensNew
 }
