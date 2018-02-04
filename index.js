7548b729545c3bdc9d7c068ce8a6076b2007d7e1
Travis Clarkson <tk1667@gmail.com>
2018-02-04 21:55:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,20 +8,40 @@ function kittens() {
   var kittens = ['Milo', 'Otis', 'Garfield']
 }
 
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
 function destructivelyAppendKitten(name) {
   kittens.push(name)
+}
+
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
 }
 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
 
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
 function destructivelyRemoveLastKitten() {
   kittens.pop()
 }
 
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
+}
+
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
 }
 
 function appendKitten(name) {
@@ -29,14 +49,26 @@ function appendKitten(name) {
   return kittensNew
 }
 
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
 function prependKitten(name) {
   var kittensNew = [name, ...kittens]
   return kittensNew
 }
 
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
 function removeLastKitten() {
   var kittensNew = kittens.slice(-1)
   return kittensNew
+}
+
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
 }
 
 function removeFirstKitten() {
