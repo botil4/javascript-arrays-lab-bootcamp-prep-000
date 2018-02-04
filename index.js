f3f319855717ddaa8d1a2911c2ad23ba118a291b
Travis Clarkson <tk1667@gmail.com>
2018-02-04 21:36:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const app = "I don't do much."
 
 window.kittens = ["Milo", "Otis", "Garfield"]
 
-function kittens() {
-  var kittens = ["Milo", "Otis", "Garfield"]
-  return kittens
-}
-
 function destructlivelyAppendKitten(name) {
   window.kittens = kittens.push(name)
 }
